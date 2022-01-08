2ea68ebb226dd8e59479f65ded2f690f9f61b5a4
yankvasya <yankvasya@gmail.com>
2022-01-08 21:36:37 +0300

    feat(form): created html template

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,37 @@
 <template>
-  <Tutorial/>
+  <div class="wrapper">
+    <div class="adding-goods">
+      <h1 class="adding-good__title">
+        <form accept-charset="UTF-8" class="form">
+          <div class="form__field field">
+            <h3 class="field__name">Наименование товара</h3>
+            <label class="field__label">
+              <input type="text" placeholder="Введите название товара">
+            </label>
+          </div>
+          <div class="form__field field">
+            <h3 class="field__name">Описание товара</h3>
+            <label class="field__label">
+              <input type="text" placeholder="Введите описание товара">
+            </label>
+          </div>
+          <div class="form__field field">
+            <h3 class="field__name">Цена товара</h3>
+            <label class="field__label">
+              <input type="text" placeholder="Введите цену">
+            </label>
+          </div>
+          <button type="submit">Добавить товар</button>
+        </form>
+      </h1>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'Main'
 }
 </script>
+
+<style lang="scss" scoped></style>
