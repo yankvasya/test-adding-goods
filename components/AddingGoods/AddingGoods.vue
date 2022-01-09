8dc6dde5dd19d3a3000d3ce408093e01fe751d53
yankvasya <yankvasya@gmail.com>
2022-01-10 00:32:00 +0300

    feat(form): added validation value in inputs

--- components/AddingGoods/AddingGoods.vue
+++ components/AddingGoods/AddingGoods.vue
@@ -3,13 +3,53 @@
 <script>
 export default {
   name: 'AddingGoods',
+  data() {
+    return {
+      values: {
+        name: { warn: false },
+        cost: { warn: false },
+        url: { warn: false }
+      },
+      isValuesEmpty: true
+    }
+  },
   methods: {
-    createGoods(e) {
+    valueChanged(e) {
+      switch (e.target.name) {
+        case 'name':
+          this.values.name.warn = !e.target.value
+          break;
+        case 'url':
+          this.values.url.warn = !e.target.value
+          break;
+        case 'cost':
+          this.values.cost.warn = !e.target.value
+          break;
+        default:
+          break;
+      }
+    },
+    submitForm(e) {
       e.preventDefault()
-      const { name, desc, url, cost } = e.target.elements
+
+      const data = e.target.elements
+      const { name, url, cost } = data
+      this.checkFormValues({name,url,cost}) && this.createGoods(data)
+    },
+    checkFormValues(elements) {
+      let isValuesFull = true
+      for (const [_, input] of Object.entries(elements)) {
+        if (!input.value) {
+          isValuesFull = false
+        }
+      }
+
+      return isValuesFull
+    },
+    createGoods({ name, desc, url, cost }) {
       const data = {
         name: name.value,
-        cost: cost.value,
+        cost: cost.value*1,
         desc: desc.value,
         url: url.value
       }
