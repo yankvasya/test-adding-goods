de6e7583fefb7bb5dea39e398993f81e827d8ae8
yankvasya <yankvasya@gmail.com>
2022-01-09 22:37:16 +0300

    feat(AddingGoods): form push data to store

--- components/AddingGoods/AddingGoods.vue
+++ components/AddingGoods/AddingGoods.vue
@@ -2,7 +2,27 @@
 
 <script>
 export default {
-  name: 'AddingGoods'
+  name: 'AddingGoods',
+  methods: {
+    createGoods(e) {
+      e.preventDefault()
+      const { name, desc, url, cost } = e.target.elements
+      const data = {
+        name: name.value,
+        cost: cost.value,
+        desc: desc.value,
+        url: url.value
+      }
+
+      this.$store.commit('goods/addGoods', data)
+     this.clearForm([name,desc,url,cost])
+    },
+    clearForm(elements) {
+      for (let element of elements) {
+        element.value = ''
+      }
+    }
+  }
 }
 </script>
 
--- store/goods.js
+++ store/goods.js
@@ -1,3 +1,11 @@
 export const state = () => ({
   items: []
 })
+
+export const mutations = {
+  addGoods (state, payload) {
+    state.items.push({
+      ...payload
+    })
+  }
+}
