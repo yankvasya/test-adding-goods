9b36cecf8fd39c1469e558c46aaee643fd500e1d
yankvasya <yankvasya@gmail.com>
2022-01-09 21:17:55 +0300

    feat(TopLine): created TopLine component

---
+++ components/TopLine/TopLine.vue
@@ -0,0 +1,9 @@
+<template src="./template.html" />
+
+<script>
+export default {
+  name: 'TopLine'
+}
+</script>
+
+<style lang="scss" src="./style.scss" scoped />
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="wrapper">
-    <header class="header">
-      <div class="container">
-        <h1 class="header__title">Добавление товара</h1>
-        <Toggler />
-      </div>
-    </header>
-
+    <TopLine />
     <main class="main">
       <AddingGoods />
       <GoodsList>
@@ -22,55 +16,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.header {
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  align-items: center;
-  flex: 1;
-  padding: 0 32px 0 32px;
-
-  width: 100%;
-  height: 83px;
-
-  .container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex: 1;
-    max-width: 100%;
-  }
-
-  &__title {
-    font-style: normal;
-    font-weight: 600;
-    font-size: 28px;
-    line-height: 35px;
-    color: #3F3F3F;
-  }
-}
-
-.wrapper {
-  max-width: 1367px;
-  padding: 0 0 10px 0;
-  background: rgba(255, 254, 251, 0.8);
-}
-
-.main {
-  padding-top: 83px;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  gap: 10px;
-}
-
-.adding-goods {
-  left: 32px;
-  top: 83px;
-  position: fixed;
-  z-index: 3;
-}
-</style>
+<style lang="scss" src="./style.scss" scoped />
