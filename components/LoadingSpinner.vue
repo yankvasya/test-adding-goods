8026894810dc34985c9065adc57893dc4b4f4721
yankvasya <yankvasya@gmail.com>
2022-01-11 17:27:04 +0300

    chore: split the styles into separate files

--- components/LoadingSpinner.vue
+++ components/LoadingSpinner.vue
@@ -5,6 +5,8 @@
 </template>
 
 <style lang="scss">
+@import '../styles/vars';
+
 .spinner {
   animation: spinner-rotate 2s linear infinite;
   z-index: 2;
@@ -12,32 +14,9 @@
   height: 50px;
 
 & .path {
-    stroke: #7BAE73;
+    stroke: $green;
     stroke-linecap: round;
     animation: spinner-dash 1.5s ease-in-out infinite;
   }
-
 }
-
-@keyframes spinner-rotate {
-  100% {
-    transform: rotate(360deg);
-  }
-}
-
-@keyframes spinner-dash {
-  0% {
-    stroke-dasharray: 1, 150;
-    stroke-dashoffset: 0;
-  }
-  50% {
-    stroke-dasharray: 90, 150;
-    stroke-dashoffset: -35;
-  }
-  100% {
-    stroke-dasharray: 90, 150;
-    stroke-dashoffset: -124;
-  }
-}
-
 </style>
