3fd01f22c7d76b14983e907b9cd4748133c44a54
yankvasya <yankvasya@gmail.com>
2022-01-11 20:13:22 +0300

    fix: fix paths

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,5 +43,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    publicPath: '/nuxt/'
+  },
+
+  router: {
+    base: './',
+    mode: 'hash'
   }
 }
